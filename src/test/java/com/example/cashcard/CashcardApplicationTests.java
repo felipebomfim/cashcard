ccd711e5a46c094c44347c875bccb80ee8b06df2
Felipe Bomfim <felipebomfim4161@gmail.com>
2025-07-20 15:07:42 -0300

    Atualizado teste do endpoint POST

--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -43,7 +43,11 @@ class CashcardApplicationTests {
 	void shouldCreateANewCashCard() {
 		CashCard newCashCard = new CashCard(null, 250.00);
 		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
-		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
