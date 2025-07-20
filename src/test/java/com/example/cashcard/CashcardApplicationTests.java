da1910005eb5b747f8c93159e4abc290045774eb
Felipe Bomfim <felipebomfim4161@gmail.com>
2025-07-20 11:29:02 -0300

    Adicionado teste para  GET com id desconhecido

--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -32,7 +32,11 @@ class CashcardApplicationTests {
 	}
 
 	@Test
-	void contextLoads() {
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
 	}
 
 }
